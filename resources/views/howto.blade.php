5541819fd5baaa5cc73ec8dad9fb05e3bf7db4aa
shinnichiro <shinnichiro.mm@icloud.com>
2020-04-19 08:47:00 +0900

    make frame of toppage and howto

---
+++ resources/views/howto.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.app")
+
+@section("content")
+
+	<p class="mb-5">てすと</p>
+
+	<a href="/">戻る</a>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends("layouts.app")
+
+@section("content")
+
+	<div class="jumbotron">
+		<div class="d-flex justify-content-center mb-5">
+			<h1>麻雀手動点数入力・解析機</h1>
+		</div>
+		<div class="d-flex justify-content-around">
+			<h4>ログイン</h4>
+			<h4>登録</h4>
+			<a href="howto"><h4>What's this?</h4></a>
+		</div>
+	</div>
+
+@endsection
